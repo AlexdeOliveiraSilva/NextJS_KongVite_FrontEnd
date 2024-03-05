9643f3a8f0078a3072c5c27a001f9d05cf790b12
Rafael Oliveira <rafaelmo@pinolab.com.br>
2024-03-05 10:09:54 -0300

    att refresh topbar

--- src/context/global.js
+++ src/context/global.js
@@ -94,6 +94,7 @@ export const GlobalProvider = ({ children }) => {
             setEventChoice,
             refreshPage,
             setRefreshPage
+
         }}>
             {children}
         </GlobalContext.Provider>
